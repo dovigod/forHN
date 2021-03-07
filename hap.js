da9089eea71e9d54632949ac4f64b28e36e252ad
서지상 <inaki96@naver.com>
2021-03-07 19:51:03 +0900

    awdwada

--- hap.js
+++ hap.js
@@ -2,8 +2,14 @@ let app;
 
 let displacementSprite;
 let displacementFilter;
+
 function initPixi() {
-	app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
+	app = new PIXI.Application({
+		width: window.innerWidth,
+		height: window.innerHeight,
+		backgroundColor: 0xbce8b1
+	});
+
 	document.body.appendChild(app.view);
 	let image = new PIXI.Sprite.from('heeni.jpeg');
 	image.width = window.innerWidth;
@@ -22,7 +28,18 @@ function initPixi() {
 
 	image.interactive = true;
 	image.on('pointerdown', handler);
+	//image.on('click', clickHandler);
 }
+/*
+const clickHandler = (e) => {
+	const eSprite = new PIXI.Sprite.from('waterdrops2.jpg');
+	const eFilter = new PIXI.filters.DisplacementFilter(eSprite);
+	app.stage.addChild(displacementSprite);
+	app.stage.filters.push(eFilter);
+	eFilter.scale.x = 1;
+	eFilter.scale.y = 1;
+};
+*/
 function animate() {
 	displacementSprite.x += 10;
 	displacementSprite.y += 4;
@@ -32,9 +49,16 @@ initPixi();
 
 function handler(event) {
 	const audio = new Audio('chime_rain.flac');
-	audio.play();
+	if (!audio.play()) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
 }
-/*
+
+const cnx = document.querySelector('canvas');
+
+cnx.style.zIndex = -3; /*
 const htm = document.querySelector('html');
 htm.addEventListener('click', handler);
 */
---
+++ opening.js
@@ -0,0 +1,48 @@
+let opens = document.getElementById('entry');
+
+function moveClouds(e) {
+	/* clouds 1 & 2 move to the left 
+    
+       clouds 3 & 4 to the right */
+
+	for (i = 1; i < 5; i++) {
+		var cloud = document.getElementById(`cloud${i}`);
+		cloud.style.transitionTimingFunction = 'ease-out';
+		cloud.style.transitionDuration = '300ms';
+		var top = window.getComputedStyle(cloud, null).getPropertyValue('top');
+
+		topValue = parseInt(top);
+		topValue = topValue - 210;
+		top = topValue + 'px';
+
+		cloud.style.top = top;
+
+		var left = window.getComputedStyle(cloud, null).getPropertyValue('left');
+		leftValue = parseInt(left);
+
+		if (i < 3) {
+			leftValue = leftValue - 210;
+		} else {
+			leftValue = leftValue + 210;
+		}
+		left = leftValue + 'px';
+
+		cloud.style.left = left;
+	}
+}
+
+const handleClick = (e) => {
+	alert('click');
+	opens.classList.append('clicked');
+};
+
+const openings = () => {
+	opens.addEventListener('click', () => {
+		alert(333);
+	});
+	opens.addEventListener('onmousemove', () => {
+		alert(222);
+	});
+};
+
+export default openings;
